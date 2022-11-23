c5b804924a793cb65be122adbfde8cb827e2ce21
Foxessies <113012253+Foxessies@users.noreply.github.com>
2022-11-23 21:36:16 +0800

    Update ShowStringDetail.java

--- ShowStringDetail.java
+++ ShowStringDetail.java
@@ -13,41 +13,33 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import twicetry.clint.Player;
 import twicetry.server.Serverlist;
-
 import java.io.DataOutputStream;
 import java.io.IOException;
 
-public class ShowStringDetail
-{
-    private Stage window = new Stage();
-    private Scene scene =  new Scene(new Pane(),300,130);
-    private Label label = new Label();
-    private Button btYes = new Button("确定");
-    private Button btNo = new Button("取消");
-    //悔棋反馈标志
-    private  int flag = 0;
+public class ShowStringDetail {
+  private Stage window = new Stage();
+  private Scene scene =  new Scene(new Pane(),300,130);
+  private Label label = new Label();
+  private Button btYes = new Button("确定");
+  private Button btNo = new Button("取消");
+  private  int flag = 0;
+  private DataOutputStream toServer;
 
-    private DataOutputStream toServer;
+  public ShowStringDetail()
+  {
+      window.setResizable(false);
+      window.initStyle(StageStyle.UNDECORATED);
+      window.setX(Player.x + 350);
+      window.setY(Player.y + 300);
+      window.setScene(scene);
+      window.initModality(Modality.APPLICATION_MODAL);
+  }
 
-    public ShowStringDetail()
-    {
-        window.setResizable(false);
-        window.initStyle(StageStyle.UNDECORATED);
-        //居中
-        window.setX(Player.x + 350);
-        window.setY(Player.y + 300);
-        window.setScene(scene);
-        window.initModality(Modality.APPLICATION_MODAL);
-    }
+  public void display(String s) {
+      label.setText(s);
+      btYes.setOnAction(e -> window.close());
 
-    public void display(String s)
-    {
-        label.setText(s);
-        btYes.setOnAction(e -> window.close());
-
-        scene.setRoot(new StackPane(new VBox(new StackPane(label),new StackPane(btYes))));
-        window.showAndWait();
-    }
-
-
+      scene.setRoot(new StackPane(new VBox(new StackPane(label),new StackPane(btYes))));
+      window.showAndWait();
+  }
 }
