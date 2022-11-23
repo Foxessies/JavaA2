4b34df4f06762bd29f1646eee84e9efeb4826623
Foxessies <113012253+Foxessies@users.noreply.github.com>
2022-11-23 21:57:03 +0800

    Update OtherConnectionTo.java

--- OtherConnectionTo.java
+++ OtherConnectionTo.java
@@ -3,11 +3,6 @@ package twicetry;
 import java.io.Closeable;
 import java.io.IOException;
 
-/**
- * 工具类  用于关闭连接
- * @author 久拥-
- *
- */
 public class OtherConnectionTo
 {
     public static void close(Closeable ... connections)
