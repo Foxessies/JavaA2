e8e0fa8c431a518b3f81ee127a6f81debf8c509c
Foxessies <113012253+Foxessies@users.noreply.github.com>
2022-11-23 21:29:46 +0800

    Update Server.java

--- server/Server.java
+++ server/Server.java
@@ -11,7 +11,6 @@ import javafx.stage.WindowEvent;
 import twicetry.IndexForGame;
 import twicetry.ShowStringDetail;
 import twicetry.clint.Player;
-
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -22,41 +21,34 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class Server extends Application implements IndexForGame {
     public static ConcurrentLinkedQueue<Socket> socketList = new ConcurrentLinkedQueue<Socket>();
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage)
-    {
-        TextArea textArea = new TextArea();
-        textArea.setEditable(false);
+    public void start(Stage primaryStage) {
+      TextArea textArea = new TextArea();
+      textArea.setEditable(false);
 
-        Scene scene = new Scene(new Pane(textArea), 650, 250);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("井字棋Server");
-        primaryStage.show();
+      Scene scene = new Scene(new Pane(textArea), 600, 200);
+      primaryStage.setScene(scene);
+      primaryStage.setTitle("井字棋Server");
+      primaryStage.show();
 
-        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>()
-        {
+      primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
             @Override
-            public void handle(WindowEvent event)
-            {
-                Player.Serverfail1();
-                Player.Serverfail2();
-                System.exit(0);
+            public void handle(WindowEvent event) {
+              Player.Serverfail1();
+              Player.Serverfail2();
+              System.exit(0);
             }
-        });
+      });
 
-        new Thread(() ->
-        {
+      new Thread(() -> {
             ServerSocket serverSocket = null;
-            try
-            {
+            try {
                 serverSocket = new ServerSocket(9520);
-                Platform.runLater(() ->
-                {
+                Platform.runLater(() -> {
                     textArea.appendText( "");
                 });
                 while (true)
@@ -67,47 +59,27 @@ public class Server extends Application implements IndexForGame {
                     });
                     Socket user1 = serverSocket.accept();
                     socketList.add(user1);
-                    Platform.runLater(() ->
-                    {
-                        textArea.appendText("               玩家一连接成功！\n");
+                    Platform.runLater(() -> {
+                        textArea.appendText("        玩家一连接成功！等待玩家二连接\n");
                     });
                     DataOutputStream toUser1 = new DataOutputStream(user1.getOutputStream());
                     toUser1.writeInt(people1);
 
                     Socket user2 = serverSocket.accept();
                     socketList.add(user2);
-                    Platform.runLater(() ->
-                    {
+                    Platform.runLater(() -> {
                         textArea.appendText("""
-                                               玩家二连接成功！
-                                __________________________________________
+                                                  玩家二连接成功！
+                                ________________________________________________
                                 """);
                     });
-                    DataOutputStream toUser2 = new DataOutputStream(user2.getOutputStream());
-                    toUser2.writeInt(people2);
-                    new Thread(new TicTacTeoGame(user1, user2)).start();
+              DataOutputStream toUser2 = new DataOutputStream(user2.getOutputStream());
+              toUser2.writeInt(people2);
+              new Thread(new TicTacTeoGame(user1, user2)).start();
                 }
-            } catch (IOException e)
-            {
+            } catch (IOException e) {
                 e.printStackTrace();
             }
-        }).start();
-    }
-
-    class MClient implements Runnable {
-        public void run() {
-            try {
-                while (true) {
-
-                    Thread.sleep(2000);
-                    for (Socket socketTemp : socketList) {
-                      }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-        }
-
+      }).start();
     }
 }
