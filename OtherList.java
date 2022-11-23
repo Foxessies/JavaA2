75e8c3feeb19cebce3a050b05c625c3752857888
Foxessies <113012253+Foxessies@users.noreply.github.com>
2022-11-23 21:35:07 +0800

    Update OtherList.java

--- OtherList.java
+++ OtherList.java
@@ -3,13 +3,11 @@ package twicetry;
 public class OtherList {
     private ListForCollection lfc;
 
-    public ListForCollection getLfc()
-    {
+    public ListForCollection getLfc() {
         return lfc;
     }
 
-    public void setLfc(ListForCollection lfc)
-    {
+    public void setLfc(ListForCollection lfc) {
         this.lfc = lfc;
     }
 }
